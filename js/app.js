2bf58f3f15a215ee3e97324a9300b951f4a2b994
Alexander <mr.savkin.alexander@yandex.ru>
2017-06-19 14:30:06 +0400

    only mail

--- js/app.js
+++ js/app.js
@@ -1,129 +1,133 @@
 
 $(document).ready(function(){
   
-  var priceAuto = document.getElementById("priceAuto");
-  var okButton = document.getElementsByClassName("ok")[0];
+    var priceAuto = document.getElementById("priceAuto");
+    var okButton = document.getElementsByClassName("ok")[0];
 
-  $.validator.addMethod('validName', function (value) {
-     var result = true;
-     var iChars = "`~!@#$%^&*()+=-[]\\\';,./{}|\":<>?"+"0123456789";
-      for (var i = 0; i < value.length; i++) {
-          if (iChars.indexOf(value.charAt(i)) != -1) {
-              return false;
-          }
-      }
-      return result;
-  }, '');
+    $.validator.addMethod('validName', function (value) {
+        var result = true;
+        var iChars = "`~!@#$%^&*()+=-[]\\\';,./{}|\":<>?"+"0123456789";
+        for (var i = 0; i < value.length; i++) {
+            if (iChars.indexOf(value.charAt(i)) != -1) {
+                return false;
+            }
+        }
+        return result;
+    }, '');
 
-  $.validator.addMethod('validNum', function (value) {
-     var result = true;
-     var iChars = "`~!@#$%^&*()+=-[]\\\';,./{}|\":<>?"+"абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"+
-                  "abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"+" ";
-      for (var i = 0; i < value.length; i++) {
-          if (iChars.indexOf(value.charAt(i)) != -1) {
-              return false;
-          }
-      }
-      return result;
-  }, '');
+    $.validator.addMethod('validNum', function (value) {
+        var result = true;
+        var iChars = "`~!@#$%^&*()+=-[]\\\';,./{}|\":<>?"+"абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"+
+                     "abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"+" ";
+        for (var i = 0; i < value.length; i++) {
+            if (iChars.indexOf(value.charAt(i)) != -1) {
+                return false;
+            }
+        }
+        return result;
+    }, '');
 
-  $.validator.addMethod('validDeposite', function (value) {
-    return value < priceAuto.value;
-  }, '');
+    $.validator.addMethod('validDeposite', function (value) {
+        return value < priceAuto.value;
+    }, '');
 
-  $("#creditForm").validate({
-    submitHandler: function(form) {
-        calcCredit();
-        okButton.className = 'ok_showen';
-    },
-    focusInvalid: false,
-    focusCleanup: true,
-    rules: {
-      name: {
-           required: true,
-           validName: true,
-           minlength: 2,
-           maxlength: 30
-      },
+    $("#creditForm").validate({
+        submitHandler: function(form) {
+            calcCredit();
+            okButton.className = 'ok_showen';
+        },
 
-      email: {
-           required: true,
-           email: true
-      },
+        focusInvalid: false,
+        focusCleanup: true,
 
-      phone: {
-           required: true,
-           validNum: true,           
-           minlength: 5,
-           maxlength: 11
-      },
+        rules: {
+            clientName: {
+                required: true,
+                validName: true,
+                minlength: 2,
+                maxlength: 30
+            },
 
-      priceAuto: {
-           required: true,
-           validNum: true,           
-           range: [10000,1000000]
-      }, 
+            email: {
+                required: true,
+                email: true
+            },
 
-      deposite: {
-           required: true,
-           validNum: true,           
-           range: [10000,1000000],
-           validDeposite: true
-      },
+            phone: {
+                required: true,
+                validNum: true,           
+                minlength: 5,
+                maxlength: 11
+           },
 
-      period: {
-           required: true,
-           validNum: true,           
-           range: [3,120]
-      },
-    },
-    invalidHandler: function() {
-        okButton.className = 'ok';
-    },
-    messages: {
-      name: {
-        required: "Введите имя пользователя",
-        validName: "Символы `~!@#$%^&*()+=-[]\\\';,./{}|\":<>? цифры и пробелы запрещены",
-        minlength: "Минимум 2 символа ",
-        maxlength: "Максимум 30 символов ",
-        remote: "Такое имя уже существует "
-      },
+            priceAuto: {
+                required: true,
+                validNum: true,           
+                range: [10000,1000000]
+            }, 
 
-      email: {
-        required: "Введите адрес ящика",
-        email: "Введите корректный адрес"
-      },
+            deposite: {
+                required: true,
+                validNum: true,           
+                range: [10000,1000000],
+                validDeposite: true
+            },
 
-      phone: {
-        required: "Укажите номер телефона",
-        validNum: "Укажите номер телефона без символов +, - и пробелов ",
-        minlength: "Минимум 5 цифр ",
-        maxlength: "Максимум 11 цифр "
-      },
+            period: {
+                required: true,
+                validNum: true,           
+                range: [3,120]
+            },
+        },
 
-      priceAuto: {
-        required: "Укажите стоимость автомобиля",
-        validNum: "Введите корректную сумму ",
-        range: "Укажите сумму от 10000 до 1000000 "
-      },
+        invalidHandler: function() {
+            okButton.className = 'ok';
+        },
 
-      deposite: {
-        required: "Укажите стоимость автомобиля",
-        validNum: "Введите корректную сумму ",
-        range: "Укажите сумму от 10000 до 1000000 ",
-        validDeposite: 'Взнос не может превышать сумму автомобиля, ты что, идиот?'
-      },
+        messages: {
+            clientName: {
+                required: "Введите имя пользователя",
+                validName: "Символы `~!@#$%^&*()+=-[]\\\';,./{}|\":<>? цифры и пробелы запрещены",
+                minlength: "Минимум 2 символа ",
+                maxlength: "Максимум 30 символов ",
+            },
 
-      period: {
-        required: "Укажите срок кредитования",
-        validNum: "Введите корректный период ",
-        range: "Укажите срок от 3 до 120 месяцев "
-      },
-    },
-    errorPlacement: function(error, element) {
-      var er = element.attr("name");
-      error.appendTo( element.parent().find("label[@for='" + er + "']").find("span") );
-    }
-  });
+            email: {
+                required: "Введите адрес ящика",
+                email: "Введите корректный адрес"
+            },
+
+            phone: {
+                required: "Укажите номер телефона",
+                validNum: "Укажите номер телефона без символов +, - и пробелов ",
+                minlength: "Минимум 5 цифр ",
+                maxlength: "Максимум 11 цифр "
+            },
+
+            priceAuto: {
+                required: "Укажите стоимость автомобиля",
+                validNum: "Введите корректную сумму ",
+                range: "Укажите сумму от 10000 до 1000000 "
+            },
+
+            deposite: {
+                required: "Укажите стоимость автомобиля",
+                validNum: "Введите корректную сумму ",
+                range: "Укажите сумму от 10000 до 1000000 ",
+                validDeposite: 'Взнос не может превышать сумму автомобиля!'
+            },
+
+            period: {
+                required: "Укажите срок кредитования",
+                validNum: "Введите корректный период ",
+                range: "Укажите срок от 3 до 120 месяцев "
+            },
+        },
+
+        errorPlacement: function(error, element) {
+            var er = element.attr("name");
+            error.appendTo( element.parent().find("label[@for='" + er + "']").find("span") );
+        }
+    });
 });
 
---
+++ js/calculate.js
@@ -0,0 +1,73 @@
+
+        var priceAuto = document.getElementById("priceAuto");
+        var deposite = document.getElementById("deposite");
+        var period = document.getElementById("period");
+        var creditForm = document.getElementById("creditForm");
+        var clientName = document.getElementById("clientName");
+        var email = document.getElementById("email");
+        var phone = document.getElementById("phone");
+        var summary = document.getElementById("summary");
+        var okButton = document.getElementsByClassName("ok")[0];
+        var success = document.getElementById("success");
+
+        function positionSliderPriceAuto() {
+            var sliderPriceAuto = document.getElementById("sliderPriceAuto");
+            sliderPriceAuto.value = priceAuto.value;
+        }
+
+        function valuePriceAuto() {
+            valueSlider = document.getElementById("sliderPriceAuto").value;
+            priceAuto.value = valueSlider;
+        }
+
+        function positionSliderDeposite() {
+            sliderDeposite = document.getElementById("sliderDeposite");
+            sliderDeposite.value = deposite.value;
+        }
+
+        function valueDeposite() {
+            valueSlider = document.getElementById("sliderDeposite").value;
+            deposite.value = valueSlider;
+        }
+
+        function positionSliderPeriod() {
+            sliderPeriod = document.getElementById("sliderPeriod");
+            sliderPeriod.value = period.value;
+        }
+
+        function valuePeriod() {
+            valueSlider = document.getElementById("sliderPeriod").value;
+            period.value = valueSlider;
+        }
+
+        function calcCredit() {
+            var payment =
+                Math.round((priceAuto.value - deposite.value)/(1 - 1/Math.pow((10000 + 1183)/10000, period.value) )/10);
+
+            summary.innerHTML = payment;
+            return payment;
+        }
+
+        function onOkbuttonClick() {
+            okButton.className = 'ok';
+
+            var payment = calcCredit();
+            var letter = clientName.value + ", вы указали номер " + phone.value + " для взятия кредита в " + priceAuto.value + 
+                         " рублей с первоначальным взносом в " + deposite.value + " рублей на срок " + period.value + 
+                         " месяцев. Ежемесячный платеж составит: " + payment + " рублей."; 
+
+
+
+
+            console.log(letter);                
+            console.log('А тут нужно что-то сделать чтобы отпрваить емайл');
+
+
+
+
+            success.innerHTML = 'Письмо отправлено';
+
+            setTimeout(function() {
+                success.innerHTML = '';                
+            }, 3000)
+        };
